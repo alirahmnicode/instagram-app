af1a08350ca9afc5c5a55586d863c5c25ec8ef4a
alirahmanicode <alirahmanicode@gmail.com>
2021-10-08 09:06:31 +0330

    create api for all posts and detail post

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "api"
 
 urlpatterns = [
-    path('all_posts' , views.all_posts , name="posts"),
+    path('all_posts/' , views.all_posts , name="posts"),
+    path('posts/<str:slug>/<int:pk>/' , views.post , name="post"),
 ]
